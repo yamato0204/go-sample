513cd44ef8f3b65a67094b5e954ebc773674bfe9
yamato0204 <tech.yama.0204@gmail.com>
2024-09-22 18:10:46 +0900

    [add] report取得処理実装

--- api/internal/domain/model/report.go
+++ api/internal/domain/model/report.go
@@ -1,11 +1,12 @@
 package model
 
 type Report struct {
-	ID           string `db:"id" json:"id"`
-	Comment      string `db:"comment" json:"comment"`
-	ThumbnailUrl string `db:"thumbnail_url" json:"thumbnail_url"`
-	AuthorID     string `db:"author_id" json:"author_id"`
-	Recipe       Recipe `db:"recipe" json:"recipe_id"`
-	CreatedAt    string `db:"created_at" json:"created_at"`
-	UpdatedAt    string `db:"updated_at" json:"updated_at"`
+	ID           string   `db:"id"`
+	Comment      string   `db:"comment"`
+	ThumbnailUrl string   `db:"thumbnail_url"`
+	UserID       string   `db:"user_id"`
+	Recipe       Recipe   `db:"recipe"`
+	Category     Category `db:"category"`
+	CreatedAt    string   `db:"created_at"`
+	UpdatedAt    string   `db:"updated_at"`
 }
--- api/internal/infra/db/report_infra.go
+++ api/internal/infra/db/report_infra.go
@@ -23,33 +23,34 @@ func NewReportRepository(db *database.DB) repo.ReportRepository {
 
 func (rr *reportRepository) GetReportByUserID(ctx context.Context, userID string) ([]*model.Report, error) {
 	var report []*model.Report
-	err := rr.db.GetContext(ctx, &report, `
+	err := rr.db.SelectContext(ctx, &report, `
 	SELECT 
 		r.id,
 		r.comment,
 		r.thumbnail_url,
-		r.author_id,
-		r.recipe_id,
+		r.user_id,
+		r.recipe_id AS "recipe.id",
 		r.created_at,
 		r.updated_at,
 
-		rc.id AS recipe_id,
-		rc.title AS recipe_title,
-		rc.thumbnail_url AS recipe_thumbnail,
-		rc.recipe,
-		rc.category_id,
-		rc.ingredient,
-		rc.created_at AS recipe_created_at,
-		rc.updated_at AS recipe_updated_at,
 
-		c.id AS category_id,
-		c.name AS category_name
+		rc.id AS "recipe.id",
+		rc.title AS "recipe.title",
+		rc.thumbnail_url AS "recipe.thumbnail_url",
+		rc.recipe AS "recipe.recipe",
+		rc.category_id AS "recipe.category.id",
+		rc.ingredient AS "recipe.ingredient",
+		rc.created_at AS "recipe.created_at",
+		rc.updated_at AS "recipe.updated_at",
+
+		c.id AS "category.id",
+		c.name AS "category.name"
 
 	FROM 
 		reports AS r
 	INNER JOIN
 		users AS u
-	ON r.author_id = u.id
+	ON r.user_id = u.id
 	INNER JOIN
 		recipes AS rc
 	ON r.recipe_id = rc.id
@@ -57,7 +58,7 @@ func (rr *reportRepository) GetReportByUserID(ctx context.Context, userID string
 		categories AS c
 	ON rc.category_id = c.id
 	WHERE
-		r.author_id = ?
+		r.user_id = ?
 	`, userID)
 
 	if errors.Is(err, sql.ErrNoRows) {
@@ -67,6 +68,8 @@ func (rr *reportRepository) GetReportByUserID(ctx context.Context, userID string
 	}
 
 	if err != nil {
+		fmt.Println("error in infra")
+		fmt.Println(err)
 		return nil, err
 	}
 
